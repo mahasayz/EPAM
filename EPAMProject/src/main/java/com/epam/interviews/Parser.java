4075e67b10c090d034e3ca3294dfbb4692301a7f
Mahbub Alam <mahasayz@gmail.com>
2015-07-08 02:54:24 +0600

    Currency formatter added along-with grouping logic for displaying average

--- EPAMProject/src/main/java/com/epam/interviews/Parser.java
+++ EPAMProject/src/main/java/com/epam/interviews/Parser.java
@@ -3,9 +3,11 @@ package com.epam.interviews;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Currency;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,6 +36,10 @@ public class Parser {
 	
 	private List<Account> accounts = new ArrayList<Account>();
 	private static Map<String, String> groups = new HashMap<String, String>();
+	private static Map<String, Double> currencyMap =
+			new HashMap<String, Double>();
+	
+	
 	
 	public void parseFile(String filename) {
 		BufferedReader br = null;
@@ -69,43 +75,55 @@ public class Parser {
 	public static void main(String[] args) {
 		Parser parser = new Parser();
 		parser.parseFile("");
+
+		/*new Currency("USD", 1.00),
+			new Currency("GBP", 1.654),
+			new Currency("CHF", 1.10),
+			new Currency("EUR", 1.35)
+	};*/
+		currencyMap.put("USD", 1.00);
+		currencyMap.put("GBP", 1.654);
+		currencyMap.put("CHF", 1.10);
+		currencyMap.put("EUR", 1.35);
 		
 		Collections.sort(parser.accounts, new CountryComparator());
 		double sum = 0;
 		int counter = 0;
-		String previous = null;
+		Account previous = null;
 		for (Account account : parser.accounts) {
 			if (account.getCountry() == null || account.getCountry().equals("")) {
-				if (previous == null || previous.equals(account.getCity()+account.getCreditRating())) {
-					System.out.println("NEW/SAME City "+account.getCity()+"|CreditRating "+account.getCreditRating()+"|Sum "+sum+"|Counter "+counter);
-					previous = account.getCity()+account.getCreditRating();
+				if (previous == null || (previous.getCity().equals(account.getCity()) && previous.getCreditRating().equals(account.getCreditRating()))) {
+					previous = account;
 					sum += account.getAmount();
 					counter++;
 				} else {
-					System.out.println("DIFF City "+account.getCity()+"|CreditRating "+account.getCreditRating()+"|Sum "+sum+"|Counter "+counter);
 					Double newSum = sum/counter;
-					System.out.println("Sum was " + newSum);
-					groups.put(previous, newSum.toString());
+					newSum = (newSum * currencyMap.get(previous.getCurrency())) / currencyMap.get("EUR");
+					if (previous.getCountry() == null || previous.getCountry().equals(""))
+						groups.put(previous.getCity()+"|"+previous.getCreditRating(), newSum.toString());
+					else
+						groups.put(previous.getCountry()+"|"+previous.getCreditRating(), newSum.toString());
 					counter = 0;
 					sum = 0;
-					previous = account.getCity()+account.getCreditRating();
+					previous = account;
 					sum += account.getAmount();
 					counter++;
 				}
 			} else {
-				if (previous == null || previous.equals(account.getCountry()+account.getCreditRating())) {
-					System.out.println("NEW/SAME Country "+account.getCountry()+"|CreditRating "+account.getCreditRating()+"|Sum "+sum+"|Counter "+counter);
-					previous = account.getCountry()+account.getCreditRating();
+				if (previous == null || (previous.getCountry().equals(account.getCountry()) && previous.getCreditRating().equals(account.getCreditRating()))) {
+					previous = account;
 					sum += account.getAmount();
 					counter++;
 				} else {
-					System.out.println("DIFF Country "+account.getCountry()+"|CreditRating "+account.getCreditRating()+"|Sum "+sum+"|Counter "+counter);
 					Double newSum = sum/counter;
-					System.out.println("Sum was " + newSum);
-					groups.put(previous, newSum.toString());
+					newSum = (newSum * currencyMap.get(previous.getCurrency())) / currencyMap.get("EUR");
+					if (previous.getCountry() == null || previous.getCountry().equals(""))
+						groups.put(previous.getCity()+"|"+previous.getCreditRating(), newSum.toString());
+					else
+						groups.put(previous.getCountry()+"|"+previous.getCreditRating(), newSum.toString());
 					sum = 0;
 					counter = 0;
-					previous = account.getCountry()+account.getCreditRating();
+					previous = account;
 					sum += account.getAmount();
 					counter++;
 				}
@@ -113,7 +131,12 @@ public class Parser {
 		}
 		
 		for (String key : groups.keySet()) {
-			System.out.println(key + " = " + groups.get(key));
+			NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+			Currency currentCurrency = Currency.getInstance("EUR");
+			currencyFormatter.setCurrency(currentCurrency);
+			currencyFormatter.setMaximumFractionDigits(2);
+			Double value = Double.parseDouble(groups.get(key));
+			System.out.println(key + " = " + currencyFormatter.format(value));
 		}
 		
 	}
