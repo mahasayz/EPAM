45120f105d809199bf73430ceb70bc96cb356908
Mahbub Alam <mahasayz@gmail.com>
2015-07-08 14:52:50 +0600

    Driver class added for solution

---
+++ EPAMProject/src/main/java/com/epam/interviews/SolutionTester.java
@@ -0,0 +1,10 @@
+package com.epam.interviews;
+
+public class SolutionTester {
+	static Solution solution;
+	
+	public static void main(String[] args) {
+		solution = new Solution();
+		solution.run();
+	}
+}
